6044f5e2bf458262d1b9e542a10afbbde737ee7c
Samin Taheri <58706708+samin-taheri@users.noreply.github.com>
2023-09-05 16:30:32 +0300

    commiting changes

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,38 +1,117 @@
-import React, { ReactNode } from 'react';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 interface ModalProps {
-  isOpen: boolean;
-  onRequestClose: () => void;
-  children: ReactNode;
+  closeModal: () => void;
+  step: number;
+  selectedOption: string;
+  selectedOptionForMoney: string;
+  handleSelectChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+  handleSelectChangeForMoney: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+  nextStep: () => void;
 }
 
-const MyModal: React.FC<ModalProps> = ({ isOpen, onRequestClose, children }) => {
-  if (!isOpen) return null;
+const Modal: React.FC<ModalProps> = ({
+  closeModal,
+  step,
+  selectedOption,
+  selectedOptionForMoney,
+  handleSelectChange,
+  handleSelectChangeForMoney,
+  nextStep,
+}) => {
+  
+  const renderContent = () => {
+    switch (step) {
+      case 1:
+        return (
+          <div>
+            <h1 className='text-xl font-semibold pt-5'>Real estate from developer from 35.000$</h1>
+            <p className="text-l font-medium pt-5">Projects on the Black Sea coast with yield up to 100% per year</p>
+            <p className="text-l font-medium  pt-2 pb-3">Answer questions and recieve a PDF presentation</p>
+          </div>
+        );
+      case 2:
+        return (
+          <div>
+            <h1 className='text-xl font-semibold pt-4'>What is the purpose of ocquiring the property</h1>
+            <select
+              id="selectOption"
+              name="selectOption"
+              value={selectedOption}
+              onChange={handleSelectChange}
+              className="mt-7 mb-5 block w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-yellow-200 focus:outline-none"
+            >
+              <option value="option1">Real estate for living</option>
+              <option value="option2">Real estate fo rinvestment</option>
+            </select>
+          </div>
+        );
+      case 3:
+        return (
+          <div>
+            <h1 className='text-xl font-semibold pt-4'>How much are you counting on?</h1>
+            <select
+              id="selectOption"
+              name="selectOption"
+              value={selectedOptionForMoney}
+              onChange={handleSelectChangeForMoney}
+              className="mt-7 mb-5 block w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-yellow-200 focus:outline-none"
+            >
+              <option value="option1">$35000 - $50000</option>
+              <option value="option2">$50000 - $100000</option>
+              <option value="option2">$100000 - $150000</option>
+              <option value="option2">$150000 and more</option>
+            </select>
+          </div>
+        );
+        case 4:
+        return (
+          <div>
+            <h1 className='text-xl font-semibold pt-4'>It is done! There is just one last step</h1>
+            <p className="text-l font-medium pt-4">Leave a messanger or phone number for contact</p>
+            <select
+              id="selectOption"
+              name="selectOption"
+              value={selectedOptionForMoney}
+              onChange={handleSelectChangeForMoney}
+              className="mt-7 mb-5 block w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-yellow-200 focus:outline-none"
+            >
+              <option value="option1">WhatsApp</option>
+              <option value="option2">Viber</option>
+              <option value="option2">Phone number</option>
+            </select>
+          </div>
+        );
+        case 5:
+        return (
+          <div className='text-center pt-20'>
+            <h1 className='text-xl font-semibold'>It is done! 🤩</h1>
+            <p className="text-l font-medium pt-5">Congratulations</p>
+          </div>
+        );
+      default:
+        return null;
+    }
+  };
+
 
   return (
     <div className="modal-overlay">
-      <div className="modal-content">
-      <button onClick={onRequestClose} className="">
-          {/* Close Icon */}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <line x1="18" y1="6" x2="6" y2="18" />
-            <line x1="6" y1="6" x2="18" y2="18" />
-          </svg>
-        </button>
-        {children}
-      </div>
-    </div>
+          <div className="modal-content" style={{ width: '500px', height: '300px' }}>
+          <span className="modal-close" onClick={closeModal}>
+          <FontAwesomeIcon icon={faTimes} />
+        </span>
+            {renderContent()}
+            <div>
+            {step < 5 && (
+              <button className="px-10 py-2 mt-4 hover:yellow-500 hover:bg-yellow-500 bg-yellow-400 text-black rounded-lg" onClick={nextStep}>Next</button>
+            )}
+          </div>
+          </div>
+        </div>
   );
 };
 
-export default MyModal;
+export default Modal;
--- src/components/right-menu.tsx
+++ src/components/right-menu.tsx
@@ -3,27 +3,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSquareCheck } from '@fortawesome/free-regular-svg-icons';
 import { faListAlt } from '@fortawesome/free-regular-svg-icons';
 import { faMessage } from '@fortawesome/free-regular-svg-icons';
-import MyModal from './modal';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import Modal from './modal';
 
 interface RightMenuProps {
   isOpen: boolean;
   onClose: () => void;
   children?: React.ReactNode;
 }
-
 const RightMenu: React.FC<RightMenuProps> = ({ isOpen, children }) => {
-  const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
-
   const [isOpen2, setIsOpen2] = useState(false);
   const [step, setStep] = useState<number>(1);
   const [selectedOption, setSelectedOption] = useState<string>('option1');
   const [selectedOptionForMoney, setSelectedOptionForMoney] = useState<string>('option1');
-
-  const [selectedOptionForContact, setSelectedOptionForContact] = useState<string>('option1');
-
-  const handleOptionChangeForContact = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSelectedOptionForContact(event.target.value);
-  };
 
   const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     setSelectedOption(event.target.value);
@@ -35,7 +27,7 @@ const RightMenu: React.FC<RightMenuProps> = ({ isOpen, children }) => {
 
   const closeModal2 = () => {
     setIsOpen2(false);
-    setStep(1); // Reset the step when closing the modal
+    setStep(1);
   };
 
   const nextStep = () => {
@@ -44,91 +36,7 @@ const RightMenu: React.FC<RightMenuProps> = ({ isOpen, children }) => {
     }
   };
 
-  const renderContent = () => {
-    switch (step) {
-      case 1:
-        return (
-          <div className='p-5'>
-            <h1 className='text-xl font-semibold'>Real estate from developer from 35.000$</h1>
-            <p className="text-l font-medium pt-4">Projects on the Black Sea coast with yield up to 100% per year</p>
-            <p className="text-l font-medium  pt-4">Answer questions and recieve a PDF presentation</p>
-          </div>
-        );
-      case 2:
-        return (
-          <div>
-            <h1 className='text-xl font-semibold'>What is the purpose of ocquiring the property</h1>
-            <select
-              id="selectOption"
-              name="selectOption"
-              value={selectedOption}
-              onChange={handleSelectChange}
-              className="mt-7 mb-5 block w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-yellow-200 focus:outline-none"
-            >
-              <option value="option1">Real estate for living</option>
-              <option value="option2">Real estate fo rinvestment</option>
-            </select>
-          </div>
-        );
-      case 3:
-        return (
-          <div>
-            <h1 className='text-xl font-semibold'>How much are you counting on?</h1>
-            <select
-              id="selectOption"
-              name="selectOption"
-              value={selectedOptionForMoney}
-              onChange={handleSelectChangeForMoney}
-              className="mt-7 mb-5 block w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-yellow-200 focus:outline-none"
-            >
-              <option value="option1">$35000 - $50000</option>
-              <option value="option2">$50000 - $100000</option>
-              <option value="option2">$100000 - $150000</option>
-              <option value="option2">$150000 and more</option>
-            </select>
-          </div>
-        );
-        case 4:
-        return (
-          <div>
-            <h1 className='text-xl font-semibold'>It is done! There is just one last step</h1>
-            <p className="text-l font-medium pt-4">Leave a messanger or phone number for contact</p>
-            <p className="text-l font-medium pt-4">You will be contacted by our manager with a ready-made offer for real estate</p>
-            <select
-              id="selectOption"
-              name="selectOption"
-              value={selectedOptionForMoney}
-              onChange={handleSelectChangeForMoney}
-              className="mt-7 mb-5 block w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-yellow-200 focus:outline-none"
-            >
-              <option value="option1">WhatsApp</option>
-              <option value="option2">Viber</option>
-              <option value="option2">Phone number</option>
-            </select>
-          </div>
-        );
-        case 5:
-        return (
-          <div>
-            <h1 className='text-xl font-semibold'>It is done! 🤩</h1>
-            <p className="text-l font-medium pt-4">Congratulations</p>
-          </div>
-        );
-      default:
-        return null;
-    }
-  };
-
-  const openModal = () => {
-    setModalIsOpen(true);
-  };
-
-  const closeModal = () => {
-    setModalIsOpen(false);
-  };
-
   const handleItemClick = (item: string) => {
-    // Handle the click event for menu items here
     console.log(`Clicked on: ${item}`);
   };
 
@@ -155,16 +63,16 @@ const RightMenu: React.FC<RightMenuProps> = ({ isOpen, children }) => {
       <div className="flex justify-center">
       <button onClick={() => setIsOpen2(true)} className="mb-10 mr-4 px-20 py-3 hover:from-yellow-600 hover:to-yellow-500 text-black rounded-lg bg-gradient-to-r from-yellow-500 to-yellow-400">Get Started</button>
       {isOpen2 && (
-        <div className="modal-overlay">
-          <div className="modal-content" style={{ width: '600px', height: '370px' }}>
-            <span className="close" onClick={closeModal2}>&times;</span>
-            {renderContent()}
-            {step < 5 && (
-              <button className="px-10 py-2 mt-4 hover:yellow-500 hover:bg-yellow-500 bg-yellow-400 text-black rounded-lg" onClick={nextStep}>Next</button>
+              <Modal
+                closeModal={closeModal2}
+                step={step}
+                selectedOption={selectedOption}
+                selectedOptionForMoney={selectedOptionForMoney}
+                handleSelectChange={handleSelectChange}
+                handleSelectChangeForMoney={handleSelectChangeForMoney}
+                nextStep={nextStep}
+              />
             )}
-          </div>
-        </div>
-      )}
       </div>
         <li className='mb-3'>
         <a className="text-gray-600 hover:text-gray-800 text-sm">Head Office: Oba District, 14.
