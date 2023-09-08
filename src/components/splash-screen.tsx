5f04ec51e322407c86af51598af3ebe459442d12
Samin Taheri <58706708+samin-taheri@users.noreply.github.com>
2023-09-08 18:11:56 +0300

    commiting changes

---
+++ src/components/splash-screen.tsx
@@ -0,0 +1,12 @@
+// components/SplashScreen.tsx
+import React from 'react';
+
+const SplashScreen: React.FC = () => {
+  return (
+    <div className="splash-screen">
+      {/* Add your logo or loading animation here */}
+    </div>
+  );
+};
+
+export default SplashScreen;
