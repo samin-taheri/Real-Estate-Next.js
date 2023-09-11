5ab4a53b5a5975860d02fa81e216c7e4ba4f664b
Samin Taheri <58706708+samin-taheri@users.noreply.github.com>
2023-09-11 11:02:24 +0300

    commiting changes

--- src/app/pages/layout.tsx
+++
@@ -1,9 +0,0 @@
-import {FC, ReactNode } from 'react';
-
-interface LayoutProps {
-    children: ReactNode
-}
-const Layout: FC<LayoutProps> = ({children}) => {
-    return <div className='bg-red-500'>{children}</div>
-}
-export default  Layout;
--- src/components/contact-info.tsx
+++ src/components/contact-info.tsx
@@ -6,7 +6,7 @@ const ContactInfo = () => {
   return(
     <div>
       <p className="ml-3 text-l text-gray-700 font-medium animate__animated animate__fadeInRight">+90 507 752 58 16</p>
-      <p className="ml-3 text-xs text-gray-500 font-medium animate__animated animate__fadeInRight">we are online</p>
+      <p className="ml-3 text-xs text-gray-500 font-medium animate__animated animate__fadeInRight"></p>
       <WhatsAppButton />
       <ScrollToTopButton />
     </div>
