49040cf56a33e986ad372b4a0c71e1b8ffc759ae
Samin Taheri <58706708+samin-taheri@users.noreply.github.com>
2023-09-22 09:57:19 +0300

    comiting changes

--- src/components/grid-card.tsx
+++ src/components/grid-card.tsx
@@ -1,40 +1,58 @@
-// import React from 'react';
-// import GridWithBackground from './grid-with-background';
+import React from 'react';
+import GridWithBackground from './grid-with-background';
 
-// const GridCard = () => {
-//   return (
-//     <section className="text-gray-600 body-font" id="section2">
-//       <div className="container px-5 py-20 mx-auto justify-center items-center">
-//         <div className="flex flex-wrap w-full mb-8 flex-col items-center text-center">
-//           <h1 className="sm:text-4xl text-2xl font-semibold title-font text-gray-700">
-//             Other Countries
-//           </h1>
-//         </div>
-//         <div className="container p-6 flex-wrap justify-center items-center">
-//           <div className="grid lg:grid-cols-2 md:grid-cols-2 gap-4">
-//             <GridWithBackground backgroundImage="/1-1.jpg" width="200px" height="215px">
-//               <h1>fjnln</h1> {/* Add your text here */}
-//             </GridWithBackground>
-          
-//             <div className="grid lg:grid-cols-2 md:grid-cols-2 gap-4">
-//               <GridWithBackground backgroundImage="/1-2.jpg" width="200px" height="215px">
-//                 <div>fjnln</div>
-//               </GridWithBackground>
-//               <GridWithBackground backgroundImage="/1-3.jpg" width="300px" height="215px">
-//                 <div>fjnln</div>
-//               </GridWithBackground>
-//               <GridWithBackground backgroundImage="/1-4.jpg" width="300px" height="215px">
-//                 <div>fjnln</div>
-//               </GridWithBackground>
-//               <GridWithBackground backgroundImage="/1-6.jpg" width="300px" height="215px">
-//                 <div>fjnln</div>
-//               </GridWithBackground>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// };
+const GridCard = () => {
+  return (
+    <section className="text-gray-600 body-font" id="section2">
+      <div className="container px-4 py-24 mx-auto justify-center">
+        <div className="flex flex-wrap w-full flex-col items-center text-center">
+          <h1 className="sm:text-4xl text-2xl font-semibold mb-5 title-font text-gray-700 -mt-10">
+            Other Countries
+          </h1>
+        </div>
+        <div className="container p-6 flex-wrap">
+        <div className="grid lg:grid-cols-2 md:grid-cols-2 gap-4">
+            <GridWithBackground
+              title="Dubai"
+              description="Abu Dhabi"
+              backgroundImage="/abu-dhabi.jpg"
+            />
+        
+          <div className="grid lg:grid-cols-2 md:grid-cols-2 gap-4 flex-row">
+            <div className='flex flex-col'>
+            <div className='mb-4'>
+            <GridWithBackground
+              title="Canada"
+              description="Ottawa"
+              backgroundImage="/canada.jpg"
+            />
+            </div>
+            <GridWithBackground
+              title="America"
+              description="Washington D.C."
+              backgroundImage="/usa.jpg"
+            />
+            </div>
+            <div className='flex flex-col'>
+            <div className='mb-4'>
+             <GridWithBackground
+              title="England"
+              description="London"
+              backgroundImage="/london.jpg"
+            />
+            </div>
+             <GridWithBackground
+              title="Germany"
+              description="Berlin"
+              backgroundImage="/berlin.jpg"
+            />
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-// export default GridCard;
+export default GridCard;
--- src/components/grid-with-background.tsx
+++ src/components/grid-with-background.tsx
@@ -1,23 +1,54 @@
-// import React, { ReactNode } from 'react';
+import React, { ReactNode } from 'react';
 
-// interface GridWithBackgroundProps {
-//   backgroundImage: string;
-//   children: ReactNode;
-//   width?: string; // Add width prop
-//   height?: string; // Add height prop
-// }
+interface GridWithBackgroundProps {
+  backgroundImage?: string;
+  children?: ReactNode;
+  width?: string;
+  height?: string;
+  title: string;
+  description: string;
+  overlayOpacity?: number;
+}
 
-// const GridWithBackground: React.FC<GridWithBackgroundProps> = ({ backgroundImage, children, width, height }) => {
-   
-//   return (
+const GridWithBackground: React.FC<GridWithBackgroundProps> = ({
+  title,
+  description,
+  backgroundImage,
+  overlayOpacity = 0.3,
+}) => {
+  const backgroundStyle: React.CSSProperties = {
+    backgroundImage: backgroundImage ? `url(${backgroundImage})` : 'none',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    position: 'relative', // Add this to make ::before work
+  };
 
-//    <div className="p-1 rounded-xl">
-//    <img src={backgroundImage} alt={backgroundImage} className="w-full h-full object-cover rounded-xl" />
-//    <div className="absolute inset-0 p-4 flex flex-col justify-end text-white">
-//      <h2 className="text-xl font-semibold">{children}</h2>
-//    </div>
-//  </div>
-//   );
-// };
+  const overlayStyle: React.CSSProperties = {
+    content: '""',
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    backgroundColor: 'black',
+    borderRadius: "15px",
+    opacity: overlayOpacity,
+    zIndex: -1, // Place the overlay behind the content
+  };
 
-// export default GridWithBackground;
+  return (
+    <div
+      className="p-6 shadow-lg-2 rounded-2xl transition-transform transform hover:scale-105 hover:shadow-xl hover:bg-yellow-50 relative"
+      style={backgroundStyle}
+    >
+      <div style={overlayStyle}></div>
+      <h2 className="text-white text-2xl font-medium title-font z-1 relative">
+        {title}
+      </h2>
+      <p className="text-white leading-relaxed text-base z-1 pb-20 relative">{description}</p>
+    </div>
+  );
+};
+
+export default GridWithBackground;
